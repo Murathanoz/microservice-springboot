454f8ae96fa7c383e7793d5375f5b238923ebbf1
murat.oz <murathan.ozan@gmail.com>
2023-01-03 13:36:21 +0300

    feat: added DiscoveryServer

---
+++ newmicroservice/discovery-server/target/classes/ServerDiscoveryApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.inventoryservice.InventoryServiceApplication;
+
+@SpringBootApplication
+public class ServerDiscoveryApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(ServerDiscoveryApplication.class, args);
+
+		}
+}
